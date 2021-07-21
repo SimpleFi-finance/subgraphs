446cb023c6221506d73d45779e69f0c92560565b
Tarun Bansal <tarunbhm@gmail.com>
2021-07-21 18:49:04 +0530

    Make Investment Token derivied field array

--- simplefi-stake-dao/src/constants.ts
+++ simplefi-stake-dao/src/constants.ts
@@ -1,46 +1,46 @@
 export namespace Blockchain {
-    export const ETHEREUM = "ETHEREUM"
-    export const BSC = "BSC"
-    export const XDAI = "XDAI"
-    export const POLYGON = "POLYGON"
-    export const OPTIMISM = "OPTIMISM"
-    export const AVALANCHE = "AVALANCE"
-    export const NEAR = "NEAR"
+  export const ETHEREUM = "ETHEREUM"
+  export const BSC = "BSC"
+  export const XDAI = "XDAI"
+  export const POLYGON = "POLYGON"
+  export const OPTIMISM = "OPTIMISM"
+  export const AVALANCHE = "AVALANCE"
+  export const NEAR = "NEAR"
 }
 
 export namespace TokenStandard {
-    export const ERC20 = "ERC20"
-    export const ERC721 = "ERC721"
-    export const ERC1155 = "ERC1155"
+  export const ERC20 = "ERC20"
+  export const ERC721 = "ERC721"
+  export const ERC1155 = "ERC1155"
 }
 
 export namespace ProtocolName {
-    export const UNISWAP_V2 = "UNISWAP_V2"
-    export const CURVE_POOL = "CURVE_POOL"
+  export const UNISWAP_V2 = "UNISWAP_V2"
+  export const CURVE_POOL = "CURVE_POOL"
 }
 
 export namespace ProtocolType {
-    export const STAKING = "STAKING"
-    export const LENDING = "LENDING"
-    export const EXCHANGE = "EXCHANGE"
-    export const INSURANCE = "INSURANCE"
-    export const STABLECOIN = "STABLECOIN"
-    export const DERIVATIVE = "DERIVATIVE"
-    export const SYNTHETIC_TOKEN = "SYNTHETIC_TOKEN"
-    export const TOKEN_MANAGEMENT = "TOKEN_MANAGEMENT"
-    export const PREDICTION_MARKET = "PREDICTION_MARKET"
+  export const STAKING = "STAKING"
+  export const LENDING = "LENDING"
+  export const EXCHANGE = "EXCHANGE"
+  export const INSURANCE = "INSURANCE"
+  export const STABLECOIN = "STABLECOIN"
+  export const DERIVATIVE = "DERIVATIVE"
+  export const SYNTHETIC_TOKEN = "SYNTHETIC_TOKEN"
+  export const TOKEN_MANAGEMENT = "TOKEN_MANAGEMENT"
+  export const PREDICTION_MARKET = "PREDICTION_MARKET"
 }
 
 export namespace PositionType {
-    export const INVESTMENT = "INVESTMENT"
-    export const DEBT = "DEBT"
+  export const INVESTMENT = "INVESTMENT"
+  export const DEBT = "DEBT"
 }
 
 export namespace TransactionType {
-    export const INVEST = "INVEST"
-    export const REDEEM = "REDEEM"
-    export const BORROW = "BORROW"
-    export const REPAY = "REPAY"
-    export const TRANSFER_IN = "TRANSFER_IN"
-    export const TRANSFER_OUT = "TRANSFER_OUT"
+  export const INVEST = "INVEST"
+  export const REDEEM = "REDEEM"
+  export const BORROW = "BORROW"
+  export const REPAY = "REPAY"
+  export const TRANSFER_IN = "TRANSFER_IN"
+  export const TRANSFER_OUT = "TRANSFER_OUT"
 }
