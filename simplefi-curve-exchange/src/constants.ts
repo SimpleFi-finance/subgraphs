5be66e96a6c7593254d792d8994ae1605a61ab98
Goran Vladika <goran.vladika@gmail.com>
2021-07-27 23:46:44 +0200

    Add EURS pool

--- simplefi-curve-exchange/src/constants.ts
+++ simplefi-curve-exchange/src/constants.ts
@@ -109,6 +109,8 @@ export const GUSD_POOL = "0x4f062658eaaf2c1ccf8c8e36d6824cdf41167956";
 export const GUSD_LP_TOKEN = "0xd2967f45c4f384deea880f807be904762a3dea07";
 export const USDN_POOL = "0x0f9cb53ebe405d49a0bbdbd291a65ff571bc83e1";
 export const USDN_LP_TOKEN = "0x4f3e8f405cf5afc05d68142f3783bdfe13811522";
+export const EURS_POOL = "0x0ce6a5ff5217e38315f87032cf90686c96627caa";
+export const EURS_LP_TOKEN = "0x194ebd173f6cdace046c53eacce9b953f28411d1";
 
 export let addressToPool = new TypedMap<string, PoolStaticInfo>();
 addressToPool.set(
@@ -137,6 +139,7 @@ addressToPool.set(RSV_POOL, new PoolStaticInfo(RSV_POOL, RSV_LP_TOKEN, 2, "META"
 addressToPool.set(UST_POOL, new PoolStaticInfo(UST_POOL, UST_LP_TOKEN, 2, "META", false, []));
 addressToPool.set(GUSD_POOL, new PoolStaticInfo(GUSD_POOL, GUSD_LP_TOKEN, 2, "META", false, []));
 addressToPool.set(USDN_POOL, new PoolStaticInfo(USDN_POOL, USDN_LP_TOKEN, 2, "META", false, []));
+addressToPool.set(EURS_POOL, new PoolStaticInfo(EURS_POOL, EURS_LP_TOKEN, 2, "PLAIN", false, []));
 
 export let lpTokenToPool = new TypedMap<string, string>();
 lpTokenToPool.set(TRIPOOL_LP_TOKEN, TRIPOOL_POOL);
@@ -156,3 +159,4 @@ lpTokenToPool.set(RSV_LP_TOKEN, RSV_POOL);
 lpTokenToPool.set(UST_LP_TOKEN, UST_POOL);
 lpTokenToPool.set(GUSD_LP_TOKEN, GUSD_POOL);
 lpTokenToPool.set(USDN_LP_TOKEN, USDN_POOL);
+lpTokenToPool.set(EURS_LP_TOKEN, EURS_POOL);
