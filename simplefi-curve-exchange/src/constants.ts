53b5cd93145bb89fbee16d03ad6d96fb1f8508a2
Goran Vladika <goran.vladika@gmail.com>
2021-07-28 20:47:45 +0200

    Add SBTC pool

--- simplefi-curve-exchange/src/constants.ts
+++ simplefi-curve-exchange/src/constants.ts
@@ -115,6 +115,8 @@ export const REN_POOL = "0x93054188d876f558f4a66b2ef1d97d16edf0895b";
 export const REN_LP_TOKEN = "0x49849c98ae39fff122806c06791fa73784fb3675";
 export const HBTC_POOL = "0x4ca9b3063ec5866a4b82e437059d2c43d1be596f";
 export const HBTC_LP_TOKEN = "0xb19059ebb43466c323583928285a49f558e572fd";
+export const SBTC_POOL = "0x7fc77b5c7614e1533320ea6ddc2eb61fa00a9714";
+export const SBTC_LP_TOKEN = "0x075b1bb99792c9e1041ba13afef80c91a1e70fb3";
 
 export let addressToPool = new TypedMap<string, PoolStaticInfo>();
 addressToPool.set(
@@ -146,6 +148,7 @@ addressToPool.set(USDN_POOL, new PoolStaticInfo(USDN_POOL, USDN_LP_TOKEN, 2, "ME
 addressToPool.set(EURS_POOL, new PoolStaticInfo(EURS_POOL, EURS_LP_TOKEN, 2, "PLAIN", false, []));
 addressToPool.set(REN_POOL, new PoolStaticInfo(REN_POOL, REN_LP_TOKEN, 2, "PLAIN", true, []));
 addressToPool.set(HBTC_POOL, new PoolStaticInfo(HBTC_POOL, HBTC_LP_TOKEN, 2, "PLAIN", false, []));
+addressToPool.set(SBTC_POOL, new PoolStaticInfo(SBTC_POOL, SBTC_LP_TOKEN, 3, "PLAIN", true, []));
 
 export let lpTokenToPool = new TypedMap<string, string>();
 lpTokenToPool.set(TRIPOOL_LP_TOKEN, TRIPOOL_POOL);
@@ -168,3 +171,4 @@ lpTokenToPool.set(USDN_LP_TOKEN, USDN_POOL);
 lpTokenToPool.set(EURS_LP_TOKEN, EURS_POOL);
 lpTokenToPool.set(REN_LP_TOKEN, REN_POOL);
 lpTokenToPool.set(HBTC_LP_TOKEN, HBTC_POOL);
+lpTokenToPool.set(SBTC_LP_TOKEN, SBTC_POOL);
