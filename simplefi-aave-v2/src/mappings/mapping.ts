ca8c54a3cb1e1b237e4433752cdbf2c84868077b
Goran Vladika <goran.vladika@gmail.com>
2021-09-03 17:08:10 +0200

    Initial commit for aave v2 subgraph

---
+++ simplefi-aave-v2/src/mappings/mapping.ts
@@ -0,0 +1,8 @@
+import {
+  AddressesProviderRegistered,
+  AddressesProviderUnregistered,
+} from "../../generated/LendingPoolAddressesProviderRegistry/LendingPoolAddressesProviderRegistry";
+
+export function handleAddressesProviderRegistered(event: AddressesProviderRegistered): void {}
+
+export function handleAddressesProviderUnregistered(event: AddressesProviderUnregistered): void {}
