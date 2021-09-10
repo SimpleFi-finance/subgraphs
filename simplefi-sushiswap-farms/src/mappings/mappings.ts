c0a6ae7e89c4886c122d614daf3d505942b6d6a9
Goran Vladika <goran.vladika@gmail.com>
2021-09-10 22:15:54 +0200

    Add deposit handler

--- simplefi-sushiswap-farms/src/mappings/mappings.ts
+++ simplefi-sushiswap-farms/src/mappings/mappings.ts
@@ -1,15 +1,37 @@
 import { Address, BigInt, log } from "@graphprotocol/graph-ts";
 
-import { MasterChefV2, LogPoolAddition } from "../../generated/MasterChefV2/MasterChefV2";
+import { MasterChefV2, LogPoolAddition, Deposit } from "../../generated/MasterChefV2/MasterChefV2";
 
 import { IRewarder } from "../../generated/MasterChefV2/IRewarder";
 
-import { SushiFarm, Token } from "../../generated/schema";
+import {
+  SushiFarm,
+  SushiFarmSnapshot,
+  FarmDeposit,
+  UserInfo,
+  Market,
+  Account,
+  Token,
+} from "../../generated/schema";
 
-import { getOrCreateERC20Token, getOrCreateMarketWithId, ADDRESS_ZERO } from "../library/common";
+import {
+  getOrCreateERC20Token,
+  getOrCreateMarketWithId,
+  getOrCreateAccount,
+  updateMarket,
+  investInMarket,
+  TokenBalance,
+  ADDRESS_ZERO,
+} from "../library/common";
 
 import { ProtocolName, ProtocolType } from "../library/constants";
 
+let oneE12: BigInt = BigInt.fromI32(10).pow(12);
+
+/**
+ *
+ * @param event
+ */
 export function handleLogPoolAddition(event: LogPoolAddition): void {
   // create and fill SushiFarm entity
   let sushiFarm = new SushiFarm(event.params.pid.toString());
@@ -22,6 +44,8 @@ export function handleLogPoolAddition(event: LogPoolAddition): void {
   sushiFarm.totalSupply = BigInt.fromI32(0);
   let inputToken = getOrCreateERC20Token(event, event.params.lpToken);
   sushiFarm.lpToken = inputToken.id;
+  sushiFarm.lastRewardBlock = BigInt.fromI32(0);
+  sushiFarm.accSushiPerShare = BigInt.fromI32(0);
   sushiFarm.save();
 
   // create market representing the farm
@@ -42,6 +66,104 @@ export function handleLogPoolAddition(event: LogPoolAddition): void {
     inputTokens,
     null,
     rewardTokens
+  );
+}
+
+/**
+ *
+ * @param event
+ * @returns
+ */
+export function handleDeposit(event: Deposit): void {
+  let sushiFarm = SushiFarm.load(event.params.pid.toHexString());
+  let sender = getOrCreateAccount(event.params.user);
+  let receiver = getOrCreateAccount(event.params.to);
+  let amount = event.params.amount;
+
+  // save new deposit entity
+  let deposit = new FarmDeposit(
+    event.transaction.hash
+      .toHexString()
+      .concat("-")
+      .concat(event.logIndex.toHexString())
+  );
+  deposit.sushiFarm = sushiFarm.id;
+  deposit.depositer = sender.id;
+  deposit.depositReceiver = receiver.id;
+  deposit.amount = amount;
+  deposit.save();
+
+  // don't update user's position for 0 value deposit
+  if (deposit.amount == BigInt.fromI32(0)) {
+    return;
+  }
+
+  // create farm snapshot
+  let snapshotId = event.transaction.hash
+    .toHexString()
+    .concat("-")
+    .concat(event.logIndex.toHexString());
+  let farmSnapshot = new SushiFarmSnapshot(snapshotId);
+  farmSnapshot.sushiFarm = sushiFarm.id;
+  farmSnapshot.allocPoint = sushiFarm.allocPoint;
+  farmSnapshot.totalSupply = sushiFarm.totalSupply;
+  farmSnapshot.timestamp = event.block.timestamp;
+  farmSnapshot.transactionHash = event.transaction.hash.toHexString();
+  farmSnapshot.transactionIndexInBlock = event.transaction.index;
+  farmSnapshot.blockNumber = event.block.number;
+  farmSnapshot.logIndex = event.logIndex;
+  farmSnapshot.save();
+
+  // update farm's LP token total supply
+  sushiFarm.totalSupply = sushiFarm.totalSupply.plus(deposit.amount);
+  sushiFarm.save();
+
+  // update market and create market snapshot
+  let masterChef = event.address.toHexString();
+  let market = Market.load(masterChef.concat("-").concat(sushiFarm.id)) as Market;
+  updateMarket(
+    event,
+    market,
+    [new TokenBalance(sushiFarm.lpToken, masterChef, sushiFarm.totalSupply)],
+    BigInt.fromI32(0)
+  );
+
+  ////// update user's position
+  let userInfo = getOrCreateUserInfo(receiver.id, sushiFarm.id);
+  userInfo.amount = userInfo.amount.plus(amount);
+  userInfo.rewardDebt = userInfo.rewardDebt.plus(
+    amount.times(sushiFarm.accSushiPerShare).div(oneE12)
+  );
+  userInfo.save();
+
+  let outputTokenAmount = amount;
+  let inputTokenAmounts: TokenBalance[] = [new TokenBalance(sushiFarm.lpToken, masterChef, amount)];
+
+  // number of reward tokens claimed by user in this transaction
+  let rewardTokenAmounts: TokenBalance[] = [];
+
+  // total number of farm ownership tokens owned by user - 0 because sushi farms don't have token
+  let outputTokenBalance = BigInt.fromI32(0);
+
+  // inputTokenBalance -> number of LP tokens that can be redeemed by accounts's gauge tokens
+  let inputTokenBalances: TokenBalance[] = [];
+  inputTokenBalances.push(new TokenBalance(sushiFarm.lpToken, masterChef, userInfo.amount));
+
+  // reward token amounts (CRV + custom tokens) claimable by user
+  let rewardTokenBalances: TokenBalance[] = [];
+  collectRewardTokenBalances(sushiFarm, receiver, rewardTokenBalances, market);
+
+  investInMarket(
+    event,
+    receiver,
+    market,
+    outputTokenAmount,
+    inputTokenAmounts,
+    rewardTokenAmounts,
+    outputTokenBalance,
+    inputTokenBalances,
+    rewardTokenBalances,
+    null
   );
 }
 
@@ -76,4 +198,33 @@ function getRewardTokens(sushiFarm: SushiFarm): Token[] {
   }
 
   return tokens;
+}
+
+/**
+ * Create UserInfo entity
+ * @param user
+ * @param farmPid
+ * @returns
+ */
+function getOrCreateUserInfo(user: string, farmPid: string): UserInfo {
+  let id = user.concat("-").concat(farmPid);
+  let userInfo = UserInfo.load(id) as UserInfo;
+
+  if (userInfo == null) {
+    userInfo = new UserInfo(id);
+    userInfo.amount = BigInt.fromI32(0);
+    userInfo.rewardDebt = BigInt.fromI32(0);
+  }
+
+  return userInfo;
+}
+
+function collectRewardTokenBalances(
+  sushiFarm: SushiFarm,
+  receiver: Account,
+  rewardTokenBalances: TokenBalance[],
+  market: Market
+) {
+  // fetch claimable amount of sushi
+  // fetch claimable a
 }
