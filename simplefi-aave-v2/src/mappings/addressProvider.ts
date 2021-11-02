e83b7ba15452b10d84c81a8217f576483b4c4c94
Goran Vladika <goran.vladika@gmail.com>
2021-11-02 16:17:04 +0100

    Track user deposits

--- simplefi-aave-v2/src/mappings/addressProvider.ts
+++ simplefi-aave-v2/src/mappings/addressProvider.ts
@@ -1,25 +1,47 @@
-import {
-  AddressesProviderRegistered,
-  AddressesProviderUnregistered,
-} from "../../generated/LendingPoolAddressesProviderRegistry/LendingPoolAddressesProviderRegistry";
+import { AddressesProviderRegistered } from "../../generated/LendingPoolAddressesProviderRegistry/LendingPoolAddressesProviderRegistry";
 
-import { ProxyCreated } from "../../generated/templates/LendingPoolAddressesProvider/LendingPoolAddressesProvider";
-import { LendingPoolAddressesProvider, LendingPool } from "../../generated/templates";
-import { LendingPoolAddressesProvider as ProviderEntity } from "../../generated/schema";
+import {
+  ProxyCreated,
+  LendingPoolUpdated,
+} from "../../generated/templates/LendingPoolAddressesProvider/LendingPoolAddressesProvider";
+
+import {
+  LendingPoolAddressesProvider as LendingPoolAddressesProviderTemplate,
+  LendingPool as LendingPoolTemplate,
+} from "../../generated/templates";
+import { LendingPoolAddressesProvider, LendingPool } from "../../generated/schema";
+
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleAddressesProviderRegistered(event: AddressesProviderRegistered): void {
   let address = event.params.newAddress;
 
   // store address provider as entity
-  let provider = new ProviderEntity(address.toHexString());
+  let provider = new LendingPoolAddressesProvider(address.toHexString());
   provider.address = address.toHexString();
   provider.save();
 
   // start indexing the address provider
-  LendingPoolAddressesProvider.create(address);
+  LendingPoolAddressesProviderTemplate.create(address);
 }
 
 export function handleProxyCreated(event: ProxyCreated): void {
   let poolId = event.params.id;
   let poolAddress = event.params.newAddress;
+
+  let lendingPool = new LendingPool(poolAddress.toHexString());
+  lendingPool.address = poolAddress.toHexString();
+  lendingPool.save();
+
+  LendingPoolTemplate.create(poolAddress);
+}
+
+export function handleLendingPoolUpdated(event: LendingPoolUpdated): void {
+  let poolAddress = event.params.newAddress;
+
+  let lendingPool = new LendingPool(poolAddress.toHexString());
+  lendingPool.address = poolAddress.toHexString();
+  lendingPool.save();
+
+  LendingPoolTemplate.create(poolAddress);
 }
--- simplefi-aave-v2/src/mappings/lendingPool.ts
+++ simplefi-aave-v2/src/mappings/lendingPool.ts
@@ -1,3 +1,22 @@
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { Deposit } from "../../generated/templates/LendingPool/LendingPool";
 
-export function handleDeposit(event: Deposit) {}
+import { Deposit as DepositEntity } from "../../generated/schema";
+
+export function handleDeposit(event: Deposit): void {
+  let amount = event.params.amount;
+  let onBehalfOf = event.params.onBehalfOf;
+  let referral = event.params.referral;
+  let reserve = event.params.reserve;
+  let user = event.params.user;
+
+  let deposit = new DepositEntity(
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toHexString()
+  );
+  deposit.amount = amount;
+  deposit.onBehalfOf = onBehalfOf.toHexString();
+  deposit.referral = BigInt.fromI32(referral);
+  deposit.reserve = reserve.toHexString();
+  deposit.user = user.toHexString();
+  deposit.save();
+}
