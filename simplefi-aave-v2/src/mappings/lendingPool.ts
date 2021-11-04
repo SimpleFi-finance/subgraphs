36417eebbb0cbacd39f841852dd22b45ba41e9f3
Goran Vladika <goran.vladika@gmail.com>
2021-11-04 15:41:59 +0100

    Update user position when funds are deposited

--- simplefi-aave-v2/src/mappings/lendingPool.ts
+++ simplefi-aave-v2/src/mappings/lendingPool.ts
@@ -1,10 +1,17 @@
-import { BigInt, ethereum, log } from "@graphprotocol/graph-ts";
+import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts";
 import {
   Deposit,
   Withdraw,
   InitReserveCall,
 } from "../../generated/templates/LendingPool/LendingPool";
-import { Deposit as DepositEntity, Withdrawal, Reserve, Token } from "../../generated/schema";
+import {
+  Deposit as DepositEntity,
+  Withdrawal,
+  Reserve,
+  Token,
+  Market,
+  UserBalance,
+} from "../../generated/schema";
 
 import { ProtocolName, ProtocolType } from "../library/constants";
 
@@ -17,6 +24,7 @@ import {
   redeemFromMarket,
   TokenBalance,
   ADDRESS_ZERO,
+  getOrCreateMarket,
 } from "../library/common";
 
 export function handleDeposit(event: Deposit): void {
@@ -35,6 +43,58 @@ export function handleDeposit(event: Deposit): void {
   deposit.reserve = reserve.toHexString();
   deposit.user = user.toHexString();
   deposit.save();
+
+  // increase user's balance of provided tokens
+  let reserveId = event.transaction.hash.toHexString() + "-" + reserve.toHexString();
+  let userBalance = getOrCreateUserBalance(deposit.onBehalfOf, reserveId);
+  userBalance.reserve = reserveId;
+  userBalance.providedTokenAmount = userBalance.providedTokenAmount.plus(deposit.amount);
+  userBalance.outputTokenAmount = userBalance.outputTokenAmount.plus(deposit.amount);
+  userBalance.save();
+
+  ////// update user's position
+
+  let market = Market.load(event.address.toHexString() + "-" + deposit.reserve) as Market;
+
+  // depositer (msg.sender)
+  let account = getOrCreateAccount(Address.fromString(deposit.user));
+
+  // amount of aTokens moved
+  let outputTokenAmount = deposit.amount;
+
+  // amount of reserve asset tokens moved
+  let inputTokensAmount: TokenBalance[] = [
+    new TokenBalance(deposit.reserve, account.id, deposit.amount),
+  ];
+
+  // number of reward tokens claimed by user in this transaction
+  let rewardTokenAmounts: TokenBalance[] = [];
+
+  // keep track of provided token amounts
+  let outputTokenBalance = userBalance.outputTokenAmount;
+
+  // inputTokenBalance -> number of tokens that can be redeemed by aToken receiver
+  let inputTokenBalances: TokenBalance[] = [];
+  inputTokenBalances.push(
+    new TokenBalance(reserve.toHexString(), deposit.onBehalfOf, userBalance.outputTokenAmount)
+  );
+
+  // reward token amounts claimable by user
+  let rewardTokenBalances: TokenBalance[] = [];
+
+  // use common function to update position and store transaction
+  investInMarket(
+    event,
+    account,
+    market,
+    outputTokenAmount,
+    inputTokensAmount,
+    rewardTokenAmounts,
+    outputTokenBalance,
+    inputTokenBalances,
+    rewardTokenBalances,
+    null
+  );
 }
 
 export function handleWithdraw(event: Withdraw): void {
@@ -85,4 +145,26 @@ export function handleInitReserveCall(call: InitReserveCall): void {
     outputTokens,
     rewardTokens
   );
+}
+
+/**
+ * Create UserBalance entity which tracks how many tokens user provided
+ * @param user
+ * @param reserveId
+ * @returns
+ */
+export function getOrCreateUserBalance(user: string, reserveId: string): UserBalance {
+  let id = user + "-" + reserveId;
+  let userBalance = UserBalance.load(id) as UserBalance;
+
+  if (userBalance == null) {
+    userBalance = new UserBalance(id);
+    userBalance.user = user;
+    userBalance.reserve = reserveId;
+    userBalance.providedTokenAmount = BigInt.fromI32(0);
+    userBalance.outputTokenAmount = BigInt.fromI32(0);
+    userBalance.save();
+  }
+
+  return userBalance;
 }
