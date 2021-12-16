9af2e3fa562a684c75e418b36211823074f907b4
Goran Vladika <goran.vladika@gmail.com>
2021-12-16 11:05:40 +0100

    Implement repay handler

--- simplefi-compound/src/mappings/cToken.ts
+++ simplefi-compound/src/mappings/cToken.ts
@@ -1,10 +1,17 @@
 import { CToken, Market, UserDepositBalance } from "../../generated/schema";
-import { AccrueInterest, Borrow, Mint, Redeem } from "../../generated/templates/CToken/CToken";
+import {
+  AccrueInterest,
+  Borrow,
+  Mint,
+  Redeem,
+  RepayBorrow,
+} from "../../generated/templates/CToken/CToken";
 import {
   borrowFromMarket,
   getOrCreateAccount,
   investInMarket,
   redeemFromMarket,
+  repayToMarket,
   TokenBalance,
   updateMarket,
 } from "../library/common";
@@ -190,6 +197,74 @@ export function handleBorrow(event: Borrow): void {
   );
 
   borrowFromMarket(
+    event,
+    borrower,
+    market,
+    outputTokenAmount,
+    inputTokensAmount,
+    [],
+    outputTokenBalance,
+    inputTokenBalances,
+    []
+  );
+}
+
+export function handleRepayBorrow(event: RepayBorrow): void {
+  let cToken = CToken.load(event.address.toHexString()) as CToken;
+
+  let accountBorrows = event.params.accountBorrows;
+  let totalBorrows = event.params.totalBorrows;
+  let repayAmount = event.params.repayAmount;
+  let borrowPayer = getOrCreateAccount(event.params.payer);
+  let borrower = getOrCreateAccount(event.params.borrower);
+
+  // update debt balance tracker
+  let userBalance = getOrCreateUserBorrowBalance(borrower.id, cToken.id);
+  userBalance.principal = accountBorrows;
+  userBalance.interestIndex = cToken.borrowIndex;
+  userBalance.save();
+
+  // update total amount borrowed
+  cToken.totalBorrows = totalBorrows;
+  cToken.save();
+
+  // update market total supply
+  let market = Market.load(cToken.id + "-BORROW") as Market;
+  let inputTokens = market.inputTokens as string[];
+  let newInputTokenBalances: TokenBalance[] = [
+    new TokenBalance(inputTokens[0], market.id, getCollateralAmountLocked(cToken.id, totalBorrows)),
+  ];
+
+  updateMarket(event, market, newInputTokenBalances, totalBorrows);
+
+  ////// update user's position
+
+  // amount of debt repaid
+  let outputTokenAmount = repayAmount;
+
+  // amount of collateral locked after debt repaid in this TX
+  let inputTokensAmount: TokenBalance[] = [
+    new TokenBalance(
+      inputTokens[0],
+      borrower.id,
+      getCollateralAmountLocked(cToken.id, repayAmount)
+    ),
+  ];
+
+  // total balance of user's debt in this market
+  let outputTokenBalance = accountBorrows;
+
+  // total amount of user's collateral locked by debt taken in this underlying token
+  let inputTokenBalances: TokenBalance[] = [];
+  inputTokenBalances.push(
+    new TokenBalance(
+      inputTokens[0],
+      borrower.id,
+      getCollateralAmountLocked(cToken.id, accountBorrows)
+    )
+  );
+
+  repayToMarket(
     event,
     borrower,
     market,
