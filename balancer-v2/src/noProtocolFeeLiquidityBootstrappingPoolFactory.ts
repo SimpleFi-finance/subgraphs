d751ef5972e9b95fb2f0bdd28064da37fd9b9bb8
Tarun Bansal <tarunbhm@gmail.com>
2021-12-24 13:22:48 +0530

    Fix New pool NoProtocolFee menifest issue

---
+++ balancer-v2/src/noProtocolFeeLiquidityBootstrappingPoolFactory.ts
@@ -0,0 +1,16 @@
+import { PoolType } from "./constants"
+import { Pool, PoolId } from "../generated/schema"
+import { LiquidityBootstrappingPool } from "../generated/templates"
+import { PoolCreated } from "../generated/NoProtocolFeeLiquidityBootstrappingPoolFactory/LiquidityBootstrappingPoolFactory"
+import { getOrCreateAccount } from "./common"
+
+export function handlePoolCreated(event: PoolCreated): void {
+  let poolId = PoolId.load(event.params.pool.toHexString())
+  let pool = Pool.load(poolId.poolId)
+  
+  pool.factory = getOrCreateAccount(event.address).id
+  pool.poolType = PoolType.LIQUIDITY_BOOTSTRAPPING_POOL
+  pool.save()
+
+  LiquidityBootstrappingPool.create(event.params.pool)
+}
