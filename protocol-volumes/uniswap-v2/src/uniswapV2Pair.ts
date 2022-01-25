ee4ae044533bbfd70ffa50090fc7c0c717a32960
Goran Vladika <goran.vladika@gmail.com>
2022-01-25 16:54:43 +0100

    Adjust code to new schema

--- protocol-volumes/uniswap-v2/src/uniswapV2Pair.ts
+++ protocol-volumes/uniswap-v2/src/uniswapV2Pair.ts
@@ -23,6 +23,7 @@ export function handleTransfer(event: Transfer): void {
     pair.save();
 
     let marketDayData = getMarketDayData(event);
+    marketDayData.outputTokenTotalBalance = pair.totalSupply;
     marketDayData.outputTokenDailyInflowVolume = marketDayData.outputTokenDailyInflowVolume.plus(
       supplyChange
     );
@@ -35,6 +36,7 @@ export function handleTransfer(event: Transfer): void {
     pair.save();
 
     let marketDayData = getMarketDayData(event);
+    marketDayData.outputTokenTotalBalance = pair.totalSupply;
     marketDayData.outputTokenDailyOutflowVolume = marketDayData.outputTokenDailyOutflowVolume.plus(
       supplyChange
     );
@@ -47,10 +49,24 @@ export function handleSync(event: Sync): void {
   pair.reserve0 = event.params.reserve0;
   pair.reserve1 = event.params.reserve1;
   pair.save();
+
+  let marketDayData = getMarketDayData(event);
+  marketDayData.inputTokenTotalBalances = [pair.reserve0, pair.reserve1];
+  marketDayData.save();
 }
 
 export function handleMint(event: Mint): void {
+  let pair = Pair.load(event.address.toHexString()) as Pair;
+
   let marketDayData = getMarketDayData(event);
+
+  marketDayData.inputTokenDailyInflow[0] = marketDayData.inputTokenDailyInflow[0].plus(
+    event.params.amount0
+  );
+  marketDayData.inputTokenDailyInflow[1] = marketDayData.inputTokenDailyInflow[1].plus(
+    event.params.amount1
+  );
+
   marketDayData.dailyTransactions = marketDayData.dailyTransactions.plus(BigInt.fromI32(1));
   marketDayData.save();
 }
@@ -62,7 +78,18 @@ export function handleBurn(event: Burn): void {
 }
 
 export function handleSwap(event: Swap): void {
+  // totals for volume updates
+  let amount0Total = event.params.amount0Out.plus(event.params.amount0In);
+  let amount1Total = event.params.amount1Out.plus(event.params.amount1In);
+
+  // update daily swap volume per token
   let marketDayData = getMarketDayData(event);
+  marketDayData.inputTokensDailySwapVolume[0] = marketDayData.inputTokensDailySwapVolume[0].plus(
+    amount0Total
+  );
+  marketDayData.inputTokensDailySwapVolume[1] = marketDayData.inputTokensDailySwapVolume[1].plus(
+    amount1Total
+  );
   marketDayData.dailyTransactions = marketDayData.dailyTransactions.plus(BigInt.fromI32(1));
   marketDayData.save();
 }
@@ -78,10 +105,17 @@ function getMarketDayData(event: ethereum.Event): MarketDayData {
     marketDayData = new MarketDayData(dayPairID);
     marketDayData.timestamp = event.block.timestamp;
     marketDayData.market = pairAddress;
-    marketDayData.inputTokensDailyVolume = [BigInt.fromI32(0), BigInt.fromI32(0)];
+    marketDayData.inputTokensDailySwapVolume = [BigInt.fromI32(0), BigInt.fromI32(0)];
+    marketDayData.inputTokenDailyInflow = [BigInt.fromI32(0), BigInt.fromI32(0)];
+    marketDayData.inputTokenDailyOutflow = [BigInt.fromI32(0), BigInt.fromI32(0)];
     marketDayData.outputTokenDailyInflowVolume = BigInt.fromI32(0);
     marketDayData.outputTokenDailyOutflowVolume = BigInt.fromI32(0);
     marketDayData.dailyTransactions = BigInt.fromI32(0);
+
+    let pair = Pair.load(pairAddress);
+    marketDayData.inputTokenTotalBalances = [pair.reserve0, pair.reserve1];
+    marketDayData.outputTokenTotalBalance = pair.totalSupply;
+    marketDayData.save();
   }
 
   return marketDayData as MarketDayData;
