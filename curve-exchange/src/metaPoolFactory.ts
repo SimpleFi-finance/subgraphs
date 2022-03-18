d33af0b3db9a36acea65c3cfe74eb70bfcacda73
Goran Vladika <goran.vladika@gmail.com>
2022-03-18 13:33:35 +0100

    Track additions of existing pools to mainnet factory

--- curve-exchange/src/metaPoolFactory.ts
+++ curve-exchange/src/metaPoolFactory.ts
@@ -1,10 +1,12 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { MetaPoolFactory } from "../generated/schema";
 import {
+  Add_existing_metapoolsCall,
   MetaPoolFactory as MetaPoolFactoryContract,
   PlainPoolDeployed,
 } from "../generated/templates/MetaPoolFactory/MetaPoolFactory";
 import { MetaPoolDeployed } from "../generated/templates/MetaPoolFactory/MetaPoolFactory";
+import { ADDRESS_ZERO } from "./common";
 import { getOrCreatePoolViaFactory } from "./curveUtil";
 
 export function handleMetaPoolDeployedEvent(event: MetaPoolDeployed): void {
@@ -37,4 +39,22 @@ export function handlePlainDeployedEvent(event: PlainPoolDeployed): void {
 
   // create new pool
   getOrCreatePoolViaFactory(event, newCurvePoolAddress, event.address);
+}
+
+export function handleAddExistingMetapoolCall(call: Add_existing_metapoolsCall): void {
+  let factory = MetaPoolFactory.load(call.to.toHexString());
+
+  // increase pool counter by the number of pools added
+  let pools = call.inputs._pools;
+  let newPoolCounter = 0;
+
+  for (let i = 0; i < pools.length; i++) {
+    if (pools[i].toHexString() == ADDRESS_ZERO) {
+      break;
+    }
+    newPoolCounter++;
+  }
+
+  factory.poolCount = factory.poolCount.plus(BigInt.fromI32(newPoolCounter));
+  factory.save();
 }
