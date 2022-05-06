e14223b9d1f6c094928508c1e7c124af22fb4fbb
Goran Vladika <goran.vladika@gmail.com>
2022-05-06 14:35:55 +0200

    Add template for RewardPool

---
+++ harvest-finance/src/rewardPool.ts
@@ -0,0 +1,17 @@
+import {
+  RewardPaid,
+  Staked,
+  Withdrawn,
+} from "../generated/templates/ProfitSharingPool/ProfitSharingPool";
+
+export function handleStaked(event: Staked): void {
+  // to do
+}
+
+export function handleWithdrawn(event: Withdrawn): void {
+  // to do
+}
+
+export function handleRewardPaid(event: RewardPaid): void {
+  // to do
+}
